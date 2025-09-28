04a3f52f1175eb7864641527eaf474ef730234b3
Bruno Bender <brunopaulobender@gmail.com>
2025-09-28 17:09:46 -0300

    feat(issue-#5): Corrigir FormatterUtils

--- services/notification_service/src/main/java/com/postech/notification_service/utils/FormatterUtils.java
+++ services/notification_service/src/main/java/com/postech/notification_service/utils/FormatterUtils.java
@@ -5,14 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class FormatterUtils {
     public static String formatDateTime(LocalDateTime dateTime) {
-        LocalDateTime startAt = LocalDateTime.now(); // seu startAt
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
-        return startAt.format(formatter);
+        return dateTime.format(formatter);
     }
 
     public static String formatDate(LocalDateTime dateTime) {
-        LocalDateTime startAt = LocalDateTime.now(); // seu startAt
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy");
-        return startAt.format(formatter);
+        return dateTime.format(formatter);
     }
 }
